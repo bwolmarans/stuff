3b3c5897377a7c83e1651a07014f45f19e230929
bwolmarans <bwolmarans@barracuda.com>
2021-06-14 15:19:38 -0700

    Update bwafaas_login.sh

--- bwafaas_login.sh
+++ bwafaas_login.sh
@@ -1,5 +1,5 @@
-[centos@ip-10-0-1-234 ~]$ cat bwafaas_login.sh
 # YOU HAVE TO SOURCE THIS FILE NOT EXECUTRE IT OR ELSE IT WILL NOT SET THE ENVIRONMENT VARIABLE
+# usage: source ./bwafaas_login.sh brett@gmail.com ixxxxxxxxxxx42
 echo ""
 echo email=$1
 echo password=$2
@@ -7,7 +7,8 @@ echo ""
 curl "https://api.waas.barracudanetworks.com/v2/waasapi/api_login/" -X POST -d "email=$1&password=$2" | tee blah.txt
 #sed 's/"/ /g' < blah.txt | awk '{print $7}' > api_key.txt
 echo ""
-sed 's/"/ /g' < blah.txt | sed -n -e 's/^.*key ://p' | sed 's/}/ /g'
+#sed 's/"/ /g' < blah.txt | sed -n -e 's/^.*key ://p' | sed 's/}/ /g' > api_key.txt
+awk -F\" '{print $6}' < blah.txt > api_key.txt
 echo ""
 echo ""
 echo "We have saved your api key to a file in this directory named api_key.txt"
@@ -23,4 +24,4 @@ echo "curl \"https://api.waas.barracudanetworks.com/v2/waasapi/applications/\" -
 echo ""
 echo "Go ahead, copy that curl line and try it right now!"
 echo ""
-[centos@ip-10-0-1-234 ~]$
+~                                             
